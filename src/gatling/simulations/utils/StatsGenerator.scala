a916cba2ae64218e8e3861681558acea325d0ef3
Jon Soloway <jon.soloway@hmcts.net>
2025-04-09 12:01:44 +0100

    Updating stats folder

--- src/gatling/simulations/utils/StatsGenerator.scala
+++ src/gatling/simulations/utils/StatsGenerator.scala
@@ -49,7 +49,7 @@ object StatsGenerator {
 
       println(s"[StatsGenerator] Request: $name, Mean Response Time: $meanResponseTime, Total Requests: $totalRequests")
 
-      createFakeSimulation(name, meanResponseTime, totalRequests, "build/")
+      createFakeSimulation(name, meanResponseTime, totalRequests, "")
     }
   }
 
