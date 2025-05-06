5f5d6e7712dc952711341ee490de3ee5440e04d4
Jon Soloway <jon.soloway@hmcts.net>
2025-05-06 19:44:14 +0100

    Removing surplus files used for testing common-performance features

--- src/gatling/simulations/utils/UserElasticSearchFeederConfig.scala
+++
@@ -1,8 +0,0 @@
-package utils
-
-import elasticSearchFeeder._
-
-object UserElasticSearchFeederConfig extends ElasticSearchFeederConfigDefaultValues(
-  //RECORDS_REQUIRED_OVERRIDE = 20
-  //OVERRIDE_ELASTICSEARCH_WITH_CSV_FILE = true
-)
